0d517faab6c6d219c6929c287aa169b320e364a8
ashok baktha <ashok@server>
2014-09-01 02:05:35 +0530

    This is a first commit.

---
+++ CS6301/src/CS6301.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CS6301.cpp
+// Author      : Life Balance
+// Version     :
+// Copyright   : All rights reserved, KITE
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
