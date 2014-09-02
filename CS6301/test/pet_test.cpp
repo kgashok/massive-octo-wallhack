07594f1af7246df7ff6c35efdaaadf9da4b27851
ashok baktha <ashok@server>
2014-09-03 02:02:05 +0530

    Added a Pet virtual destructor test.

--- CS6301/test/pet_test.cpp
+++ CS6301/test/pet_test.cpp
@@ -22,6 +22,22 @@ public:
 };
 
 
+TEST_F(DogTest, CatPass)
+{
+	ASSERT_TRUE(true);
+
+}
+
+TEST_F(DogTest, CatPass2)
+{
+	const char* n = "Kittie";
+	c =  new Cat (n);
+
+	ASSERT_EQ("Kittie the Cat says: Meow! Meow!\n", c->MakeSound2());
+
+}
+
+
 TEST_F(DogTest, DogPass)
 {
 	ASSERT_TRUE(true);
@@ -47,24 +63,17 @@ TEST_F(DogTest, Destructors)
 	Dog* a_dog = new Dog("Frankie");
 	delete a_dog;
 
+
+}
+
+TEST_F(DogTest, DestructorPet)
+{
+
 	// When you uncomment the following lines, the compiler complains.
 	// What do you need to do to fix this?
 	// Pet* a_pet = new Pet("NoName");
 	// delete a_pet;
 
+	ASSERT_TRUE(false);  // change this to 'true' after you've compiled successfully
 }
 
-TEST_F(DogTest, CatPass)
-{
-	ASSERT_TRUE(true);
-
-}
-
-TEST_F(DogTest, CatPass2)
-{
-	const char* n = "Kittie";
-	c =  new Cat (n);
-
-	ASSERT_EQ("Kittie the Cat says: Meow! Meow!\n", c->MakeSound2());
-
-}
